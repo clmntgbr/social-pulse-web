05ec1dfaa98e6bf7ae0ecb85d97c024a4585305a
clmngbr <clement.goubier@gmail.com>
2024-12-10 22:30:30 +0100

    [master] some fixes

--- components/library/Sidebar/SidebarSocialNetworks.tsx
+++ components/library/Sidebar/SidebarSocialNetworks.tsx
@@ -18,15 +18,22 @@ export function SidebarSocialNetworks() {
           </div>
         )}
         {socialNetworks.socialNetworks?.map((item: SocialNetwork) => (
-          <SidebarMenuItem key={crypto.randomUUID()} className="hover:bg-accent rounded-xl">
+          <SidebarMenuItem key={crypto.randomUUID()} className="hover:bg-accent rounded-sm">
             <SidebarMenuButton asChild>
-              <div className="flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground h-8 text-sm">
+              <div className="flex w-full items-center gap-2 overflow-hidden rounded-sm p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground h-8 text-sm">
                 <Image
                   src={`${item.avatarUrl}`}
                   alt={item.username}
-                  width={20}
-                  height={20}
-                  className="flex-shrink-0 w-5 h-5 rounded-sm object-cover overflow-hidden"
+                  width={28}
+                  height={28}
+                  className="flex-shrink-0 w-7 h-7 rounded-full object-cover overflow-hidden"
+                />
+                <Image
+                  src={`/images/${item.socialNetworkType}-logo.png`}
+                  alt={item.username}
+                  width={10}
+                  height={10}
+                  className="absolute -bottom-[0px] left-[25px] flex-shrink-0 w-3 h-3 rounded-sm object-cover overflow-hidden"
                 />
                 <span className="font-medium">{item.username}</span>
               </div>
--- store/client/interface/social-network.ts
+++ store/client/interface/social-network.ts
@@ -1,7 +1,30 @@
-export interface SocialNetwork {
+export type SocialNetwork = {
   uuid: string;
   socialNetworkId: string;
   socialNetworkType: string;
   avatarUrl: string;
   username: string;
-}
+  createdAt: string;
+  updatedAt: string;
+  followers: boolean;
+  followings: boolean;
+  shares: boolean;
+  comments: boolean;
+  likes: boolean;
+  name: string;
+  email: string | null;
+  token: string;
+  organization: string;
+  verified: boolean;
+} & Partial<LinkedinSocialNetwork> &
+  Partial<FacebookSocialNetwork>;
+
+type LinkedinSocialNetwork = {
+  language: string | null;
+  country: string | null;
+};
+
+type FacebookSocialNetwork = {
+  website: string | null;
+  link: string | null;
+};
